608f5440a750cdc3aceb7d8c5a2767f874c4214e
Damola Adewunmi <damolasd@gmail.com>
2020-10-02 07:48:40 +0100

    Added UI to build pages

---
+++ lib/car_owners_page.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+
+import 'car_owner.dart';
+
+class CarOwnersPage extends StatelessWidget {
+  CarOwnersPage({Key key, this.owners}) : super(key: key);
+  List<CarOwner> owners;
+
+  final primary = Color(0xFF696B9E);
+  final secondary = Color(0xFFF29A94);
+  final TextStyle dropdownMenuItem =
+      TextStyle(color: Colors.black, fontSize: 18);
+
+  Widget ownerCard(BuildContext context, int index) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.white,
+        ),
+        width: double.infinity,
+        height: 230,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  owners[index].fullName,
+                  style: TextStyle(
+                      color: primary,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
+                SizedBox(height: 8),
+                Row(
+                  children: [
+                    Icon(Icons.email, color: secondary, size: 15),
+                    SizedBox(width: 5),
+                    Text(owners[index].email,
+                        style: TextStyle(
+                            color: primary, fontSize: 10, letterSpacing: .1)),
+                    SizedBox(width: 8),
+                    Icon(Icons.person, color: secondary, size: 15),
+                    SizedBox(width: 5),
+                    Text(owners[index].gender,
+                        style: TextStyle(
+                            color: primary, fontSize: 10, letterSpacing: .1)),
+                  ],
+                ),
+                SizedBox(height: 6),
+                Row(
+                  children: [
+                    Icon(Icons.directions_car, color: secondary, size: 15),
+                    SizedBox(width: 5),
+                    RichText(
+                        text: TextSpan(
+                            text: owners[index].carColor + " ",
+                            style: TextStyle(
+                                color: primary, fontSize: 8, letterSpacing: .1),
+                            children: [
+                          TextSpan(
+                            text: owners[index].carModelYear.toString() + " ",
+                            style: TextStyle(
+                                color: primary, fontSize: 8, letterSpacing: .1),
+                          ),
+                          TextSpan(
+                            text: owners[index].carModel,
+                            style: TextStyle(
+                                color: primary, fontSize: 8, letterSpacing: .1),
+                          ),
+                        ])),
+                    SizedBox(width: 10),
+                    Icon(Icons.work, color: secondary, size: 15),
+                    SizedBox(width: 5),
+                    Text(owners[index].jobTitle,
+                        style: TextStyle(
+                            color: primary, fontSize: 8, letterSpacing: .1)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.location_city, color: secondary, size: 15),
+                    SizedBox(width: 5),
+                    Text(owners[index].country,
+                        style: TextStyle(
+                            color: primary, fontSize: 8, letterSpacing: .3)),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.book, color: secondary, size: 15),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Text(owners[index].bio,
+                        style: TextStyle(
+                            color: primary, fontSize: 9, letterSpacing: .3)),
+                  ],
+                )
+              ],
+            ))
+          ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFF0F0F0),
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(top: 145),
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  child: ListView.builder(
+                      itemCount: owners.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ownerCard(context, index);
+                      }),
+                ),
+                Container(
+                  height: 140,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      color: primary,
+                      borderRadius: BorderRadius.only(
+                          bottomRight: Radius.circular(30),
+                          bottomLeft: Radius.circular(30))),
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon:
+                                  Icon(Icons.arrow_back, color: Colors.white)),
+                          Text(
+                            "Car Owners",
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.menu, color: Colors.white))
+                        ],
+                      )),
+                ),
+                Container(
+                    child: Column(
+                  children: [
+                    SizedBox(
+                      height: 110,
+                    ),
+                    Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Material(
+                            elevation: 5.0,
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            child: TextField(
+                              cursorColor: Theme.of(context).primaryColor,
+                              style: dropdownMenuItem,
+                              decoration: InputDecoration(
+                                  hintText: "Search Full Name",
+                                  hintStyle: TextStyle(
+                                      color: Colors.black38, fontSize: 16),
+                                  prefixIcon: Material(
+                                    elevation: 0.0,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(30)),
+                                    child: Icon(Icons.search),
+                                  ),
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.symmetric(
+                                      horizontal: 25, vertical: 13)),
+                            )))
+                  ],
+                ))
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/filter_display.dart
+++ lib/filter_display.dart
@@ -1,99 +1,134 @@
-
+import 'package:damolaAdewunmi/car_owner.dart';
+import 'package:damolaAdewunmi/car_owners_page.dart';
 import 'package:flutter/material.dart';
 
 import 'filter.dart';
 
-class FilterBoxList extends StatelessWidget { 
-   final List<Filter> items;
-   FilterBoxList({Key key, this.items}); 
-   
-   @override 
-   Widget build(BuildContext context) {
-      return ListView.builder(
-         itemCount: items.length,
-         itemBuilder: (context, index) {
-            return GestureDetector(
-               child: FilterBox(item: items[index]), 
-               onTap: () {
-                  // Navigator.push(
-                  //    context, MaterialPageRoute(
-                  //       builder: (context) => FilterPage(item: items[index]), 
-                  //    ), 
-                  // ); 
-               }, 
-            ); 
-         }, 
-      ); 
-   } 
+class FilterBoxList extends StatelessWidget {
+  final List<Filter> items;
+  List<CarOwner> owners;
+  FilterBoxList({Key key, this.items, this.owners});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          child: FilterBox(item: items[index]),
+          onTap: () {
+            var filteredOwners = filterData(owners, items[index]);
+            print(owners);
+            print(filteredOwners);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CarOwnersPage(owners: filteredOwners),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+List<CarOwner> filterData(List<CarOwner> owners, Filter fil) {
+  return owners
+      .where((owner) => owner.carModelYear >= fil.startYear)
+      .where((owner) => owner.carModelYear <= fil.endYear)
+      .where((owner) => fil.gender.length > 0
+          ? owner.gender.toLowerCase() == fil.gender.toLowerCase()
+          : true)
+      .where((owner) => fil.countries.length > 0
+          ? fil.countries.any(
+              (country) => owner.country.toLowerCase() == country.toLowerCase())
+          : true)
+      .where((owner) => fil.colors.length > 0
+          ? fil.colors.any(
+              (color) => owner.carColor.toLowerCase() == color.toLowerCase())
+          : true)
+      .toList();
 }
 
 class FilterBox extends StatelessWidget {
-   FilterBox({Key key, this.item}) : super(key: key); 
-   final Filter item; 
-   
-   Widget build(BuildContext context) {
-      return Container(
-         padding: EdgeInsets.all(2), height: 140, 
-         child: Card(
-            child: Row( 
-               mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-               children: <Widget>[
-                  Expanded( 
-                     child: Container( 
-                        padding: EdgeInsets.all(5), 
-                        child: Column( 
-                           mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-                           children: <Widget>[ 
-                              Text(this.item.startYear.toString(), style:TextStyle(fontWeight: FontWeight.bold)), 
-                              Text(this.item.endYear.toString()), 
-                              Text(this.item.gender),
+  FilterBox({Key key, this.item}) : super(key: key);
+  final Filter item;
+  final primary = Color(0xFF696B9E);
+  final secondary = Color(0xFFF29A94);
+  final TextStyle dropdownMenuItem =
+      TextStyle(color: Colors.black, fontSize: 18);
 
-                              Text("colors:" +this.item.colors[0]),
-                           ], 
-                        )
-                     )
-                  )
-               ]
-            ), 
-         )
-      ); 
-   } 
+  Widget getItems(String shown, List data) {
+    List<Widget> wid = new List<Widget>();
+    wid.add(new Text(shown + ": ", 
+    style: TextStyle(color: primary, fontSize: 10)
+    ));
+    if (data.length == 0) {
+      wid.add(new Text("All", style: TextStyle(color: secondary, fontSize: 9)));
+    }
+    for(var i = 0; i < data.length; i++) {
+      wid.add(new Text(
+       i == data.length - 1 ? data[i] :  data[i] + " , ",
+        style: TextStyle(color: secondary, fontSize: 9)
+      ));
+    }
+    return new Wrap(
+      children: wid
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(25),
+          color: Colors.white,
+        ),
+        width: double.infinity,
+        height: 100,
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  RichText(
+                    text: TextSpan(
+                        text: "Date Range: ",
+                        style: TextStyle(color: primary, fontSize: 10),
+                        children: [
+                          TextSpan(
+                            text: this.item.startYear.toString() + " - ",
+                            style: TextStyle(color: secondary, fontSize: 10),
+                          ),
+                          TextSpan(
+                            text: this.item.endYear.toString(),
+                            style: TextStyle(color: secondary, fontSize: 10),
+                          )
+                        ]),
+                  ),
+                  SizedBox(height: 5),
+                  RichText(
+                    text: TextSpan(
+                        text: "Gender: ",
+                        style: TextStyle(color: primary, fontSize: 10),
+                        children: [
+                          TextSpan(
+                            text: this.item.gender.length == 0 ? "All" : this.item.gender ,
+                            style: TextStyle(color: secondary, fontSize: 10),
+                          )
+                        ]),
+                  ),
+                  SizedBox(height: 5),
+                  //Get Countries
+                  getItems("Colors", this.item.colors),
+                  SizedBox(height: 5),
+                  getItems("Countries", this.item.countries)
+                ]))
+          ],
+        ));
+  }
 }
-
-// class FilterPage extends StatelessWidget { 
-//    FilterPage({Key key, this.item}) : super(key: key); 
-//    final Filter item; 
-//    @override 
-//    Widget build(BuildContext context) {
-//       return Scaffold(
-//          appBar: AppBar(title: Text(this.item.name),), 
-//          body: Center( 
-//             child: Container(
-//                padding: EdgeInsets.all(0), 
-//                child: Column( 
-//                   mainAxisAlignment: MainAxisAlignment.start, 
-//                   crossAxisAlignment: CrossAxisAlignment.start, 
-//                   children: <Widget>[
-//                      Image.asset("assets/appimages/" + this.item.image), 
-//                      Expanded( 
-//                         child: Container( 
-//                            padding: EdgeInsets.all(5), 
-//                            child: Column( 
-//                               mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-//                               children: <Widget>[ 
-//                                  Text(this.item.name, style: 
-//                                     TextStyle(fontWeight: FontWeight.bold)), 
-//                                  Text(this.item.description), 
-//                                  Text("Price: " + this.item.price.toString()), 
-                                 
-//                               ], 
-//                            )
-//                         )
-//                      ) 
-//                   ]
-//                ), 
-//             ), 
-//          ), 
-//       ); 
-//    } 
-// }
