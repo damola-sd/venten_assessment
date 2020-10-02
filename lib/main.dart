26cbd8d49e1b9869d5399aa27ee87ff00fe3fbef
Damola Adewunmi <damolasd@gmail.com>
2020-10-02 07:47:32 +0100

    Built filter function: added some UI elements as well

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:csv/csv.dart';
 
+import 'car_owner.dart';
 import 'filter.dart';
 import 'filter_display.dart';
 
@@ -14,6 +17,7 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   Future<List<Filter>> filters;
+  List<CarOwner> carOwners;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -34,14 +38,18 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'My Filters', filters: filters,),
+      home: MyHomePage(
+          title: 'My Filters', filters: filters, carOwners: carOwners),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   Future<List<Filter>> filters;
-  MyHomePage({Key key, this.title, this.filters}) : super(key: key);
+  List<CarOwner> carOwners;
+
+  MyHomePage({Key key, this.title, this.filters, this.carOwners})
+      : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -60,18 +68,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Future<List<Filter>> filters;
+  List<CarOwner> carOwners;
 
-  List<List<dynamic>> car_owners_data = [];
-
+  final primary = Color(0xFF696B9E);
+  final secondary = Color(0xFFF29A94);
+  final TextStyle dropdownMenuItem =
+      TextStyle(color: Colors.black, fontSize: 18);
 
   @override
   void initState() {
     super.initState();
     filters = fetchFilters();
+    carOwners = new List<CarOwner>();
+    loadCsv();
   }
+
   loadCsv() async {
-    final carOwners = await rootBundle.loadString('assets/car_ownsers_data.csv');
-    print(carOwners);
+    final data = await rootBundle.loadString('venten/car_ownsers_data.csv');
+    // final data2 = File.readAsLinesSync('venten/car_ownsers_data.csv')
+    List<List<dynamic>> csvData = CsvToListConverter().convert(data);
+    // print(csvData);
+    var owners = new List<CarOwner>();
+    for (var i = 1; i < csvData.length; i++) {
+      var tempy = CarOwner.fromList(csvData[i]);
+      owners.add(tempy);
+    }
+    carOwners = owners;
+    setState(() {});
   }
 
   @override
@@ -83,35 +106,39 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: FutureBuilder<List<Filter>>(
-        future: filters, builder: (context, snapshot) {
-                  if (snapshot.hasError) print(snapshot.error); 
-                  return snapshot.hasData ? FilterBoxList(items: snapshot.data) :
-                  
-                  // return the ListView widget : 
-                  Center(child: CircularProgressIndicator()); 
-        },
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      backgroundColor: Color(0xFFF0F0F0),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: FutureBuilder<List<Filter>>(
+                      future: filters,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) print(snapshot.error);
+                        return snapshot.hasData
+                            ? FilterBoxList(
+                                items: snapshot.data, owners: carOwners)
+                            :
+
+                            // return the ListView widget :
+                            Center(child: CircularProgressIndicator());
+                      },
+                    ),
+                );
   }
 }
 
-List<Filter> parseFilters(String responseBody) { 
-   final parsed = json.decode(responseBody).cast<Map<String, dynamic>>(); 
-   return parsed.map<Filter>((json) =>Filter.fromMap(json)).toList(); 
-} 
+List<Filter> parseFilters(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Filter>((json) => Filter.fromMap(json)).toList();
+}
 
 Future<List<Filter>> fetchFilters() async {
   final data = await http.get('https://ven10.co/assessment/filter.json');
   if (data.statusCode == 200) {
     return parseFilters(data.body);
-
-  }else {
+  } else {
     throw Exception('Failed to load filters');
   }
 }
