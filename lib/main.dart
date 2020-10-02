4d3579e9f4c650e97df6981a38975fe1b8ad66b7
Damola Adewunmi <damolasd@gmail.com>
2020-10-02 10:41:54 +0100

    made changes: Changed appbar color, fixed minor build errors

--- lib/main.dart
+++ lib/main.dart
@@ -110,6 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
+          backgroundColor: primary,
         ),
         body: FutureBuilder<List<Filter>>(
                       future: filters,
